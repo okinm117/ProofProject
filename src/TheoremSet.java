3237c802b4a674611845b0fbd4122b1e9455ef83
okinm117 <niko@mcn.org>
2013-07-25 17:42:33 -0700

    W/ Inference Checker

--- src/TheoremSet.java
+++ src/TheoremSet.java
@@ -1,26 +1,27 @@
 import java.util.Hashtable;
+import java.util.LinkedList;
 
 public class TheoremSet{
-	
-	public Hashtable<String,String> myTheorems;
-	
+
+	public Hashtable<String,LinkedList<String>> myTheorems;
+
 	public TheoremSet ( ) {
-		myTheorems = new Hashtable<String,String>();
+		myTheorems = new Hashtable<String,LinkedList<String>>();
 	}
 
 	public void put (String s, Expression e) {
-		myTheorems.put(s,e.toString());
+		myTheorems.put(s,e.Queue);
 	}
-	
-	public void put (String s, String e) {
-		myTheorems.put(s,e);
-	}
-	
-	public String get (String s)
+
+	//public void put (String s, String e) {
+	//	myTheorems.put(s,e);
+	//}
+
+	public LinkedList<String> get (String s)
 	{
 		return myTheorems.get(s);
 	}
-	
+
 	public boolean contains (String s)
 	{
 		return myTheorems.containsValue(s);
