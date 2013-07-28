ed0a142a773b08d53836711f4806070885584a4e
irongavel <tohmajudge@berkeley.edu>
2013-07-27 17:34:44 -0700

    Update Proof.java
    FUCK MULTIPLE LETTER EXPRESSIONS! MWAHAHAHAHHAHAHA

--- src/Proof.java
+++ src/Proof.java
@@ -688,7 +688,7 @@ public class Proof {
 							{true,true,false,true,false,false,true},
 							{false,false,true,false,true,false},
 							{true,true,false,true,false,false,true},
-							{true,true,false,true,true,false,true},
+							{true,true,false,true,false,false,true},
 							{false,false,true,false,true,false,false},
 							{false,false,false,false,false,true,false}};
 
