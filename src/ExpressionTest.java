29a922647418337d45f5144d52f321d2028f4706
irongavel <tohmajudge@berkeley.edu>
2013-07-28 15:31:44 -0700

    Use ALLL THE TILDES

--- src/ExpressionTest.java
+++ src/ExpressionTest.java
@@ -17,6 +17,9 @@ public class ExpressionTest {
 		
 		//Check basic operations parsing correctly for each operation
 		
+		Expression n = new Expression("a");
+		assertEquals(n.toString(),"a");
+		
 		Expression n1 = new Expression("(a=>b)");
 		assertEquals(n1.toString(),"=>ab");
 		
@@ -40,6 +43,55 @@ public class ExpressionTest {
 		
 		Expression n6 = new Expression("(((~a=>b)=>(b=>c))|(~b=>~a))");
 		assertEquals(n6.toString(),"|=>=>~ab=>bc=>~b~a");
+		
+		//Check basic operations parsing correctly with ~
+		
+		Expression n7 = new Expression("(~~a=>b)");
+		assertEquals(n7.toString(),"=>~~ab");
+		Expression n71 = new Expression("~(a=>b)");
+		assertEquals(n71.toString(),"~=>ab");
+		Expression n72 = new Expression("(a=>~b)");
+		assertEquals(n72.toString(),"=>a~b");
+		
+		Expression n8 = new Expression("(~(a=>b)&(a=>b))");
+		assertEquals(n8.toString(),"&~=>ab=>ab");
+		Expression n81 = new Expression("((a=>b)&~(a=>b))");
+		assertEquals(n81.toString(),"&=>ab~=>ab");
+		Expression n82 = new Expression("~((a=>b)&(a=>b))");
+		assertEquals(n82.toString(),"~&=>ab=>ab");
+		Expression n83 = new Expression("((a=>b)&(a=>~b))");
+		assertEquals(n83.toString(),"&=>ab=>a~b");
+		Expression n84 = new Expression("((~a=>b)&(a=>b))");
+		assertEquals(n84.toString(),"&=>~ab=>ab");
+	
+		Expression n9 = new Expression("(~(a=>b)|(a=>b))");
+		assertEquals(n9.toString(),"|~=>ab=>ab");
+		Expression n91 = new Expression("((a=>b)|~(a=>b))");
+		assertEquals(n91.toString(),"|=>ab~=>ab");
+		Expression n92 = new Expression("~((a=>b)|(a=>b))");
+		assertEquals(n92.toString(),"~|=>ab=>ab");
+		Expression n93 = new Expression("((a=>b)|(a=>~b))");
+		assertEquals(n93.toString(),"|=>ab=>a~b");
+		Expression n94 = new Expression("((~a=>b)|(a=>b))");
+		assertEquals(n94.toString(),"|=>~ab=>ab");
+		
+		//Testing nested Expression with ~ in most positions
+		Expression d1 = new Expression("~(~(~(~a=>~b)=>~(~b=>~c))=>(~b=>~a))");
+		assertEquals(d1.toString(),"~=>~=>~=>~a~b~=>~b~c=>~b~a");
+		
+		//Testing nested Expression with ~~ in most positions
+		Expression d2 = new Expression("~~(~~(~~(~~a=>~~b)=>~~(~~b=>~~c))=>(~~b=>~~a))");
+		assertEquals(d2.toString(),"~~=>~~=>~~=>~~a~~b~~=>~~b~~c=>~~b~~a");
+		
+		//Testing nested Expression with ~~~ in most positions
+		Expression d3 = new Expression("~~~(~~~(~~~(~~~a=>~~~b)=>~~~(~~~b=>~~~c))=>(~~~b=>~~~a))");
+		assertEquals(d3.toString(),"~~~=>~~~=>~~~=>~~~a~~~b~~~=>~~~b~~~c=>~~~b~~~a");
+		//Testing nested Expression with ~~~ in most positions, &
+		Expression d4 = new Expression("~~~(~~~(~~~(~~~a=>~~~b)=>~~~(~~~b=>~~~c))&(~~~b=>~~~a))");
+		assertEquals(d4.toString(),"~~~&~~~=>~~~=>~~~a~~~b~~~=>~~~b~~~c=>~~~b~~~a");
+		//Testing nested Expression with ~~~ in most positions, |
+		Expression d5 = new Expression("~~~(~~~(~~~(~~~a=>~~~b)=>~~~(~~~b=>~~~c))&(~~~b=>~~~a))");
+		assertEquals(d5.toString(),"~~~&~~~=>~~~=>~~~a~~~b~~~=>~~~b~~~c=>~~~b~~~a");
 		
 	}
 
