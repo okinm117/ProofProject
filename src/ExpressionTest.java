4f6a9497c87702c77216dd7b1581712fb22ea08d
derekrazo <derekrazo@gmail.com>
2013-07-28 14:41:49 -0700

    Update ExpressionTest.java

--- src/ExpressionTest.java
+++ src/ExpressionTest.java
@@ -5,36 +5,41 @@ import org.junit.Test;
 
 public class ExpressionTest {
 
-  @Test
+	@Test
 	public void test() {
 		
 		/*
 		 * This cluster of tests check to make sure that the expressions 
-		 * are being parsed in the expected way, from the top level down.
+		 * are being parsed in the expected way, from the top level down,
+		 * taking into account the various operations possible.
 		 *
 		 * */
 		
-		Expression n = new Expression("(a=>b)");
-		assertEquals(n.toString(),"=>ab");
+		//Check basic operations parsing correctly for each operation
+		
+		Expression n1 = new Expression("(a=>b)");
+		assertEquals(n1.toString(),"=>ab");
 		
 		Expression n2 = new Expression("((a=>b)&(a=>b))");
-		//assertEquals(n2,"&=>ab=>ab");
+		assertEquals(n2.toString(),"&=>ab=>ab");
+	
+		Expression n3 = new Expression("((a=>b)|(a=>b))");
+		assertEquals(n3.toString(),"|=>ab=>ab");
 		
-		System.out.print("n2 is equal to: " + n2.Queue);
+		//System.out.print("n is equal to: " + n1);
+		//System.out.print("n2 is equal to: " + n2);
+		//System.out.print("n3 is equal to: " + n3);
+
+		//Test nested parsing happening correctly for each operation
 		
-		Expression n3 = new Expression("((a=>b)&(a=>b))");
-		//assertEquals(n3.toString(),"=>ab");
+		Expression n4 = new Expression("(((a=>b)=>(b=>c))=>(b=>a))");
+		assertEquals(n4.toString(),"=>=>=>ab=>bc=>ba");
 		
-		System.out.print("n3 is equal to: " + n3.Queue);
+		Expression n5 = new Expression("(((~a=>b)=>(b=>c))&(~b=>~a))");
+		assertEquals(n5.toString(),"&=>=>~ab=>bc=>~b~a");
 		
-		Expression n4 = new Expression("(a=>b)");
-		assertEquals(n4.toString(),"=>ab");
-		
-		Expression n5 = new Expression("(a=>b)");
-		assertEquals(n5.toString(),"=>ab");
-		
-		Expression n6 = new Expression("(a=>b)");
-		assertEquals(n6.toString(),"=>ab");
+		Expression n6 = new Expression("(((~a=>b)=>(b=>c))|(~b=>~a))");
+		assertEquals(n6.toString(),"|=>=>~ab=>bc=>~b~a");
 		
 	}
 
